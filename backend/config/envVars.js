caf94a0aa464730a86423080895829cf4fe9dafa
AdityaVikramSingh45 <adityavikramshubh234@gmail.com>
2025-05-21 19:40:50 +0530

    Env_Variable change

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -3,9 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV_VARS = {
-	MONGO_URI: process.env.MONGO_URI,
+	MONGO_URI: process.env.MONGO_URL,
 	PORT: process.env.PORT || 5000,
 	JWT_SECRET: process.env.JWT_SECRET,
 	NODE_ENV: process.env.NODE_ENV,
-	TMDB_API_KEY: process.env.TMDB_API_KEY,
+	RAPIDAPI_KEY: process.env.RAPIDAPI_KEY,
+	RAPIDAPI_KEY: process.env.RAPIDAPI_KEY,
+	PEXEL_API_KEY: process.env.PEXEL_API_KEY,
 };
