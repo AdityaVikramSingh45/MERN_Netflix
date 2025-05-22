1faa6ef63438fad9edfcdc9e6822c111a9baebd5
AdityaVikramSingh45 <adityavikramshubh234@gmail.com>
2025-05-22 14:20:38 +0530

    first commit

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -1,13 +1,12 @@
-import dotenv from "dotenv";
+const dotenv = require("dotenv");
 
 dotenv.config();
 
-export const ENV_VARS = {
-	MONGO_URI: process.env.MONGO_URL,
+exports.ENV_VARS = {
+	MONGO_URL: process.env.MONGO_URL,
 	PORT: process.env.PORT || 5000,
 	JWT_SECRET: process.env.JWT_SECRET,
 	NODE_ENV: process.env.NODE_ENV,
-	RAPIDAPI_KEY: process.env.RAPIDAPI_KEY,
 	RAPIDAPI_KEY: process.env.RAPIDAPI_KEY,
 	PEXEL_API_KEY: process.env.PEXEL_API_KEY,
 };
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const searchRoute = require("../backend/Routes/search")
 const cookieParser = require("cookie-parser");
 const { protectRoute } = require("./middlwares/protectRoute");
 const cors = require("cors");
+const path = require("path");
+// const __dirname = path.resolve();
+const ENV_VARS = require("../backend/config/envVars");
 
 const dotenv = require("dotenv");
 dotenv.config(); 
@@ -23,18 +26,28 @@ app.use(cors({
 
 app.use(express.json()) // will allow to parse req.body
 app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+// 👉 Serve static files (React frontend)
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
  
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/movie", protectRoute, movieRoute);
 app.use("/api/v1/tv", protectRoute, tvRoute);
 app.use("/api/v1/search", protectRoute, searchRoute);
 
+if(ENV_VARS.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 const PORT = process.env.PORT || 3001;
 
-app.get("/", (req, res)=>{
-    res.send("Home Page");
-})
+// app.get("/", (req, res)=>{
+//     res.send("Home Page");
+// })
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
