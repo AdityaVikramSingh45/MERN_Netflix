06e7c383567990817ba4cf8c8cee2137a54e4c6c
AdityaVikramSingh45 <adityavikramshubh234@gmail.com>
2025-05-23 17:04:28 +0530

    ...

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const shimmerArray = new Array(8).fill(0);
+console.log("base Url", baseURL)
 
 const SearchPage = () => {
   const [activeTab, setActiveTab] = useState("movie");
